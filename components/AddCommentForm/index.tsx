5a48d89cd0832d0aeeb24fa9b814c2938cc9ad0a
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-16 23:13:55 +0300

    фикс

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -4,20 +4,20 @@ import styles from "./AddCommentForm.module.scss";
 
 const AddCommentForm = () => {
   const [active, serActive] = React.useState(false);
-  const [input, setInput] = React.useState('')
-  
+  const [input, setInput] = React.useState("");
+
   const onAdd = () => {
     serActive(false);
-    setInput('');
-  }
+    setInput("");
+  };
   const onChange = (evt) => {
     setInput(evt.target.value);
-  }
+  };
   return (
     <div className={styles.container}>
       <Input
-      value={input}
-      onChange={onChange}
+        value={input}
+        onChange={onChange}
         onFocus={() => serActive(true)}
         minRows={active ? 5 : 1}
         classes={{ root: styles.input }}
@@ -25,9 +25,16 @@ const AddCommentForm = () => {
         fullWidth
         multiline
       ></Input>
-       {active && <Button className={styles.button} variant="contained" color="primary" onClick={onAdd}>
+      {active && (
+        <Button
+          className={styles.button}
+          variant="contained"
+          color="primary"
+          onClick={onAdd}
+        >
           Опубликовать
-        </Button>}
+        </Button>
+      )}
     </div>
   );
 };
