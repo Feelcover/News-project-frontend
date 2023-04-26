8e813a7a7a7c0d22df785a42ac008032b4883d84
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-26 23:20:23 +0300

    стили и верстка для header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,53 +1,43 @@
-import React from "react";
-import {
-  Paper,
-  Button,
-  IconButton,
-  Avatar,
-  List,
-  ListItem,
-} from "@material-ui/core";
+import React from 'react';
+import { Paper, Button, IconButton, Avatar, List, ListItem } from '@material-ui/core';
 import {
   Search as SearchIcon,
   BorderColor as CreateIcon,
   ChatBubbleOutline as MessageIcon,
-  NotificationsNone as NotificationIcon
-} from "@mui/icons-material";
-import styles from "./Header.module.scss";
+  NotificationsNone as NotificationIcon,
+  Menu as MenuIcon,
+} from '@mui/icons-material';
+import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
   return (
     <Paper classes={{ root: styles.paper }}>
-      <div>
-        <a>
-          <img
-            height={35}
-            className={styles.logo}
-            src="/static/img/logo.jpg"
-            alt="Logo"
-          />
-        </a>
-        <div className={styles.searchContainer}>
-          <SearchIcon />
-          <input placeholder="Поиск" />
+        <div className={styles.headerLeft}>
+          <IconButton><MenuIcon/></IconButton>
+          <a>
+            <img height={35} className={styles.logo} src="/static/img/logo.jpg" alt="Logo" />
+          </a>
+          <div className={styles.searchContainer}>
+            <SearchIcon />
+            <input placeholder="Поиск" />
+          </div>
+          <Button>
+            <CreateIcon />
+          </Button>
         </div>
-        <Button>
-          <CreateIcon />
-        </Button>
-      </div>
-      <div>
-        <IconButton>
-          <MessageIcon />
-        </IconButton>
-        <IconButton>
-          <NotificationIcon />
-        </IconButton>
-        <Avatar
-          className={styles.avatar}
-          alt="Remy Sharp"
-          src="https://i.playground.ru/p/vOQQQO7dUl7oVvbUNJlDJQ.png?255x255"
-        />
-      </div>
+        <div className={styles.headerRight}>
+          <IconButton>
+            <MessageIcon />
+          </IconButton>
+          <IconButton>
+            <NotificationIcon />
+          </IconButton>
+          <Avatar
+            className={styles.avatar}
+            alt="Remy Sharp"
+            src="https://i.playground.ru/p/vOQQQO7dUl7oVvbUNJlDJQ.png?255x255"
+          />
+        </div>
     </Paper>
   );
 };
