e08d08dd03a7eb23b16886496a16877b4d476fb0
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-27 16:09:25 +0300

    часть меню

--- components/LeftSideBar/index.tsx
+++ components/LeftSideBar/index.tsx
@@ -1,19 +1,33 @@
-import { Button } from '@material-ui/core'
-import WhatshotTwoToneIcon from "@mui/icons-material/WhatshotTwoTone";
-import React from 'react'
+import { Button } from '@material-ui/core';
+import WhatshotTwoToneIcon from '@mui/icons-material/WhatshotTwoTone';
+import {
+  Search as SearchIcon,
+  BorderColor as CreateIcon,
+  SmsOutlined as MessageIcon,
+  NotificationsNone as NotificationIcon,
+  Menu as MenuIcon,
+} from '@mui/icons-material';
+import React from 'react';
 import styles from './LeftSideBar.module.scss';
 
 export const LeftSideBar = () => {
   return (
-    <div className={styles.menu}>
-      <ul>
+    <div className={styles.LeftSideBar}>
+      <ul className={styles.menu}>
         <li>
           <Button>
-            <WhatshotTwoToneIcon/>
-            Лента</Button>
+            <WhatshotTwoToneIcon />
+            <p className={styles.menuItems}>Лента</p>
+            
+          </Button>
+        </li>
+        <li>
+          <Button>
+            <MenuIcon />
+            <p className={styles.menuItems}>Сообщения</p>
+          </Button>
         </li>
       </ul>
-
     </div>
-  )
-}
+  );
+};
