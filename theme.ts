c65cc3ebd591ddffca65211ea2314428ec751d20
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-28 17:34:49 +0300

    правка theme

--- theme.ts
+++ theme.ts
@@ -1,11 +1,6 @@
 import { createTheme } from '@material-ui/core';
 
 export const theme = createTheme({
-  props: {
-    MuiButtonBase: {
-      disableRipple: true,
-    },
-  },
   palette: {
     primary: {
       main: '#4683d9',
@@ -38,12 +33,6 @@ export const theme = createTheme({
           backgroundColor: 'white',
           boxShadow:
             '0 1px 1px rgb(0 0 0 / 18%), 0 4px 7px rgb(0 0 0 / 8%), 0 -1px 0 rgb(0 0 0 / 8%), -1px 0 0 rgb(0 0 0 / 8%), 1px 0 0 rgb(0 0 0 / 15%)',
-        },
-      },
-      containedPrimary: {
-        backgroundColor: '#4683d9',
-        '&:hover': {
-          backgroundColor: '#437CCE',
         },
       },
     },
