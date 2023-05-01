76a9fbc8b1c3cfb1e8f338988ecd6d5fce61229f
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-01 16:23:51 +0300

    переход на страницу по клику и заготовка

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,9 +1,13 @@
+import { TextField } from '@material-ui/core'
 import React from 'react'
 import { MainLayout } from '../layouts/MainLayout'
 
 const write = () => {
   return (
     <MainLayout hideComments>
+        <div style={{backgroundColor:"F2F2F2"}}>]
+        <TextField placeholder='заголовок'></TextField>
+        </div>
         </MainLayout>
   )
 }
