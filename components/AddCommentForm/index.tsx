2be0198968c5db6f12ec79b18fe2203cbbca28e0
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-03 22:39:47 +0300

    правка стилей

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Input } from "@material-ui/core";
 import styles from "./AddCommentForm.module.scss";
 
-
 const AddCommentForm = () => {
-  return (<div>
-    <Input classes={{root:styles.input}} placeholder="Написать комментарий..." fullWidth multiline></Input>
-  </div>);
+  return (
+    <div>
+      <Input
+        minRows={5}
+        classes={{ root: styles.input }}
+        placeholder="Написать комментарий..."
+        fullWidth
+        multiline
+      ></Input>
+    </div>
+  );
 };
 
 export default AddCommentForm;
