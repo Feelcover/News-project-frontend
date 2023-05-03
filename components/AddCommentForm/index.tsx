421edb431aa3ee00ea7fade20af26a0db4884a6f
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-03 22:56:40 +0300

    кнопка опубликовать

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Input } from "@material-ui/core";
+import { Button, Input } from "@material-ui/core";
 import styles from "./AddCommentForm.module.scss";
 
 const AddCommentForm = () => {
   const [active, serActive] = React.useState(false);
   return (
-    <div>
+    <div className={styles.container}>
       <Input
         onFocus={() => serActive(true)}
         minRows={active ? 5 : 1}
@@ -14,6 +14,9 @@ const AddCommentForm = () => {
         fullWidth
         multiline
       ></Input>
+       {active && <Button className={styles.button} variant="contained" color="primary">
+          Опубликовать
+        </Button>}
     </div>
   );
 };
--- pages/write.tsx
+++ pages/write.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import WriteArea from '../components/WriteArea/WriteArea'
 import { MainLayout } from '../layouts/MainLayout'
 
+
 const write = () => {
   return (
     <MainLayout className="layout-white" hideComments hideMenu>
