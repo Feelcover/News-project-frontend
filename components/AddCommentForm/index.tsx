52288ccd71e155180dfba3252a4bdcd6654f429b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-03 23:08:01 +0300

    управляемый инпут

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -4,9 +4,21 @@ import styles from "./AddCommentForm.module.scss";
 
 const AddCommentForm = () => {
   const [active, serActive] = React.useState(false);
+  const [input, setInput] = React.useState('')
+  console.log(input);
+  
+  const onAdd = () => {
+    serActive(false);
+    onChange('');
+  }
+  const onChange = (evt) => {
+    setInput(evt.target.value);
+  }
   return (
     <div className={styles.container}>
       <Input
+      value={input}
+      onChange={onChange}
         onFocus={() => serActive(true)}
         minRows={active ? 5 : 1}
         classes={{ root: styles.input }}
