3a7790915ea3e7fea00de06d3d9f52163441c354
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-12 16:45:34 +0300

    rename

--- components/SideComments/index.tsx
+++ components/SideComments/index.tsx
@@ -11,8 +11,10 @@ export const SideComments = () => {
   const [active, setActive] = React.useState(true);
 
   return (
-    <div className={styles.root} >
-      <h3 onClick={() => setActive(!active)}>Комментарии {active ? <ArrowRightIcon /> : <ArrowLeftIcon />}</h3>
+    <div className={styles.root}>
+      <h3 onClick={() => setActive(!active)}>
+        Комментарии {active ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+      </h3>
       {active &&
         data.comments.popular.map((obj) => (
           <CommentItem key={obj.id} {...obj} />
