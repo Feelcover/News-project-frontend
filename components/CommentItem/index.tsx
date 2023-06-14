53a7baa34c20adf08038dff9f125cef107c24eb6
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-14 23:45:17 +0300

    корректировка пути ссылок

--- components/CommentItem/index.tsx
+++ components/CommentItem/index.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./CommentItem.module.scss";
 
 interface CommentItemProps {
   user: {
+    id: string;
     fullname: string;
     avatarUrl: string;
   };
@@ -20,10 +22,12 @@ export const CommentItem: React.FC<CommentItemProps> = ({
   return (
     <div className={styles.commentItem}>
       <div className={styles.userInfo}>
-        <img src={user.avatarUrl} />
-        <a href="#">
-          <b>{user.fullname}</b>
-        </a>
+        <img src={user.avatarUrl} alt="avatar" />
+        <Link href={`/profile/${user.id}`}>
+          <a href="#">
+            <b>{user.fullname}</b>
+          </a>
+        </Link>
       </div>
       <p className={styles.text}>{text}</p>
       <a href="#">
--- data.js
+++ data.js
@@ -49,7 +49,7 @@ module.exports = {
     ],
     "new": [
       {
-        "id": 3,
+        "id": 4,
         "user": {
           "id": 1,
           "fullname": "Вася Пупкин",
