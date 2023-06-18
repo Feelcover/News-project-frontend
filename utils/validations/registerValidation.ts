2756e5b3cf8b23d6ea02b31189d5f16855c026e4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-18 23:12:27 +0300

    фикс RegisterFormValidation

--- utils/validations/registerValidation.ts
+++ utils/validations/registerValidation.ts
@@ -1,7 +1,10 @@
 import * as yup from "yup";
 
 export const RegisterFormValidation = yup.object().shape({
-    name: yup.string().min(2, 'Не менее 2 символов').required('Введите ФИО'),
-    email: yup.string().email('Неверный формат почты').required('Введите почту'),
-    password: yup.string().min(6, 'Не менее 5 символов').required('Введите пароль'),
+  name: yup.string().min(2, "Не менее 2 символов").required("Введите ФИО"),
+  email: yup.string().email("Неверный формат почты").required("Введите почту"),
+  password: yup
+    .string()
+    .min(6, "Не менее 5 символов")
+    .required("Введите пароль"),
 });
