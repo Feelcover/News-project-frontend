22b0e54195d547bcc3383c702465f707daa29ba5
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-18 23:12:54 +0300

    фикс LoginFormValidation

--- utils/validations/loginValidation.ts
+++ utils/validations/loginValidation.ts
@@ -1,6 +1,9 @@
 import * as yup from "yup";
 
 export const LoginFormValidation = yup.object().shape({
-    email: yup.string().email('Неверный формат почты').required('Введите почту'),
-    password: yup.string().min(6, 'Не менее 5 символов').required('Введите пароль'),
+  email: yup.string().email("Неверный формат почты").required("Введите почту"),
+  password: yup
+    .string()
+    .min(6, "Не менее 5 символов")
+    .required("Введите пароль"),
 });
