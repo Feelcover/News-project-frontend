9d52ed7f6398e96a5c6b3178d3a183cc724568cb
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-19 23:33:49 +0300

    подготовка для использования контекста react-hook-form

--- components/AuthForms/FormField/FormField.tsx
+++ components/AuthForms/FormField/FormField.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useFormContext } from "react-hook-form";
 
 const FormField = () => {
+  const {register, formState} = useFormContext();
+  
   return (
     <TextField
-    {...register("email")}
-    error={}
-    helperText={}
-    className="mb-20"
-    name="email"
-    size="small"
-    label="Почта"
-    variant="outlined"
-    fullWidth
-    required
-  />
-  )
-}
+      {...register("email")}
+      error={}
+      helperText={}
+      className="mb-20"
+      name="email"
+      size="small"
+      label="Почта"
+      variant="outlined"
+      fullWidth
+      required
+    />
+  );
+};
 
-export default FormField
+export default FormField;
