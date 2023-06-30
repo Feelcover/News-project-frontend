c70c9c0c19c2546b0f00f783bf91ba617317a6fc
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-30 14:22:56 +0300

    редактирование валидации и формы

--- components/AuthForms/Forms/RegisterForm.tsx
+++ components/AuthForms/Forms/RegisterForm.tsx
@@ -5,6 +5,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { RegisterFormValidation } from "../../../utils/validations/registerValidation";
 import FormField from "../FormField/FormField";
+import { log } from "console";
 
 interface RegisterFormProps {
   backTo: () => void;
@@ -21,7 +22,7 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ backTo }) => {
     <div>
       <FormProvider {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField name="name" label="ФИО" required={true} />
+          <FormField name="fullName" label="ФИО" required={true} />
           <FormField name="email" label="Почта" required={true} />
           <FormField name="password" label="Пароль" required={true} />
           <div className={styles.mailButtonWrapper}>
--- utils/validations/registerValidation.ts
+++ utils/validations/registerValidation.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const RegisterFormValidation = yup.object().shape({
-  name: yup.string().min(2, "Не менее 2 символов").required("Введите ФИО"),
+  fullName: yup.string().min(2, "Не менее 2 символов").required("Введите ФИО"),
   email: yup.string().email("Неверный формат почты").required("Введите почту"),
   password: yup
     .string()
