8d5fee6ed99a3dba6e35f56155358beacc298626
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-07-05 23:41:47 +0300

    фикс register api

--- utils/api/index.ts
+++ utils/api/index.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
-import { LoginUser, RegisterUser } from "./types";
+import { LoginUser, RegisterUser, ResRegisterUser } from "./types";
 
 const instance = axios.create({
   baseURL: "http://localhost:3001",
 });
 
 export const UserApi = {
-  async register(registerUser: RegisterUser) {
+  async register(registerUser: RegisterUser): Promise<ResRegisterUser> {
     const { data } = await instance.post("auth/register", registerUser);
     return data;
   },
 
-  async login (loginUser: LoginUser) {
+  async login(loginUser: LoginUser) {
     const { data } = await instance.post("auth/login", loginUser);
     return data;
   },
