84dc0ae24163e23943ba7027c5009afb8fc35d21
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-07-05 23:22:58 +0300

    обьединение типов

--- utils/api/types.ts
+++ utils/api/types.ts
@@ -1,10 +1,8 @@
-export type RegisterUser = {
-    fullName: string;
-    email: string;
-    password: string;
-}
-
 export type LoginUser = {
     email: string;
     password: string;
 }
+
+export type RegisterUser = {
+    fullName: string;
+} & LoginUser;
